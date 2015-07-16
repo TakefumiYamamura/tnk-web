f7764e3a58386edce2861804201a27fc57f62b0a
TakeumiYamamura <yama1223xxx@gmail.com>
2015-07-16 21:33:13 +0900

    メアド変更

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,6 @@
 class InquiryMailer < ActionMailer::Base
   default from: "yama1223xxx@gmail.com"   # 送信元アドレス
-  default to: "tnk.12th.recruit@gmail.com"     # 送信先アドレス tnk.12th.recruit@gmail.com
+  default to: "tnk.startup@gmail.com"     # 送信先アドレス tnk.12th.recruit@gmail.com
 
   def received_email(inquiry)
     @inquiry = inquiry
