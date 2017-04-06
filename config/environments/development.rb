719d4595960f59a1a9fa2bfe4bb19c36e1e73577
Kunimaru <kunimaru@keio.jp>
2017-04-06 15:41:40 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'localhost:3000',
-    :user_name            => 'htm197',
+    :domain               => 'gmail.com',
+    :user_name            => 'htm197@gmail.com',
     :password             => 'ffrflraxqsyjwidc',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
