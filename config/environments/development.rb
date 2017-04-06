a95215572eaaeb55d98c8f2afa6084282236ee0a
Kunimaru <kunimaru@keio.jp>
2017-04-06 15:37:05 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,10 +19,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'localhost:3000',
-    :user_name            => 'yama1223xxx',
-    :password             => 'pop010918',
-    # :password             => 'fqnskvlegahrvogw',
+    :domain               => 'gmail.com',
+    :user_name            => 'htm197',
+    :password             => 'ffrflraxqsyjwidc',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
   # Print deprecation notices to the Rails logger.
